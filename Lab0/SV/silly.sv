9014c27fad6f6ba76c691b86ea168efcbb797d21
emeliakravchuk <148596593+emeliakravchuk@users.noreply.github.com>
2024-09-09 15:19:43 -0500

    Update silly.sv
    comments

--- Lab0/SV/silly.sv
+++ Lab0/SV/silly.sv
@@ -2,11 +2,12 @@ module silly (input  logic a, b, c, output logic sum, cout);
    
   assign y = ~b & ~c | a & ~b;
    
-endmodule
+endmodule  //silly module given to students as an example of coding for the logic design
+
 module fulladder (input logic a, b, c, output logic sum, cout);
 
 assign sum=a^b^c;
 
 assign cout=a&b|a&c|b&c;
 
-endmodule
+endmodule //actual module used in lab 0 to produce a full adder on the FPGA
