edb540a4cce046c3d1e5d9fb7c0af72925a717b2
emeliakravchuk <148596593+emeliakravchuk@users.noreply.github.com>
2024-09-12 14:54:30 -0500

    Update silly.sv

--- Lab1/SV/silly.sv
+++ Lab1/SV/silly.sv
@@ -1,5 +1,7 @@
-module silly (input  logic a, b, c, output logic y);
-   
-  assign y = ~b & ~c | a & ~b;
-   
-endmodule
+module fulladder (input logic a, b, c, output logic sum, cout);
+
+assign sum=a^b^c;
+
+assign cout=a&b|a&c|b&c;
+
+endmodule //1-bit full adder module 
