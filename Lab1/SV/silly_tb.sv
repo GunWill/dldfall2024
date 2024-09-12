9687efaab8491344599fd36bed981d20a72d6cd1
emeliakravchuk <148596593+emeliakravchuk@users.noreply.github.com>
2024-09-12 16:24:40 -0500

    Update silly_tb.sv

--- Lab1/SV/silly_tb.sv
+++ Lab1/SV/silly_tb.sv
@@ -10,15 +10,14 @@ module tb ();
    logic        clk;  
 
 
-
+assign sum_correct = a + b + c;
+assign cout_correct = a&b | a&c | b&c;
+assign error_sum = sum != sum_correct; 
+assign error_out = cout != cout_correct;
    
   // instantiate device under test
 	
-	fulladder_1 dut (a, b, c, sum, cout);
-	fulladder FA0 dut(a[0], b[0], c, sum[0], c0);
-  	fulladder FA1 dut(a[1], b[1], c0, sum[1], c1);
-  	fulladder FA2 dut(a[2], b[2], c1, sum[2], c2);
-  	fulladder FA3 dut(a[3], b[3], c2, sum[3], cout);
+ripple_carry_adder dut( a, b,  c, sum, cout);
 	
 	
    	//clock
@@ -34,59 +33,29 @@ integer i;
 
 	initial 
 		begin
-			handle3 = $fopen("rca.out");
+			handle3 = $fopen("rca.out"); //generating a file
 			desc3 = handle3;
 			#1250 $finish; 
 		end
 
 initial
 	begin
-		for(i=0; i<175; i+i+1)
+		for(i=0; i<175; i=i+1)
 			begin
+
+				@(posedge clk)
+				begin
+				a = $random;
+				b = $random;
+				end
+
+				@(negedge clk)
+				begin
+					$fdisplay(desc3, "%h %h || %h | %h | %b", a, b, sum, sum_correct, (sum==sum_correct));
+				end
+			end
 				
 
-   initial
-     begin
-
-	//all inputs have a 20ns delay
-
-	//testing truth table of the fulladder 
-    
-	#20  a = 1'b0;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b1;		
-
-
-	
-     end
 
    
 endmodule
