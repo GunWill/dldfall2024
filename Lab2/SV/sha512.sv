6736bd857fbdba6a353ed703a5f589e278f52ad6
emeliakravchuk <148596593+emeliakravchuk@users.noreply.github.com>
2024-10-11 23:57:58 -0500

    Update sha512.sv

--- Lab2/SV/sha512.sv
+++ Lab2/SV/sha512.sv
@@ -47,8 +47,8 @@ module sha256 #(parameter PADDED_SIZE = 1024)
 	//need 80 K values of 64 bits for each values, now how to get that values?
 	//5120
 	
-   logic [2047:0] K = {32'h428a2f98, 32'h71374491, 32'hb5c0fbcf,
-		       32'he9b5dba5, 32'h3956c25b, 32'h59f111f1, 32'h923f82a4,
+	logic [2047:0] K = {64'h428a2f98d728ae22, 64'h7137449123ef65cd, 64'hb5c0fbcfec4d3b2f,
+		       64'he9b5dba58189dbbc, 64'h3956c25bf348b538, 32'h59f111f1, 32'h923f82a4,
 		       32'hab1c5ed5, 32'hd807aa98, 32'h12835b01, 32'h243185be,
 		       32'h550c7dc3, 32'h72be5d74, 32'h80deb1fe, 32'h9bdc06a7,
 		       32'hc19bf174, 32'he49b69c1, 32'hefbe4786, 32'h0fc19dc6,
@@ -64,6 +64,47 @@ module sha256 #(parameter PADDED_SIZE = 1024)
 		       32'h682e6ff3, 32'h748f82ee, 32'h78a5636f, 32'h84c87814,
 		       32'h8cc70208, 32'h90befffa, 32'ha4506ceb, 32'hbef9a3f7,
 		       32'hc67178f2};
+
+
+
+
+
+
+
+
+
+64'h428a2f98d728ae22, 64'h7137449123ef65cd, 65'hb5c0fbcfec4d3b2f, 65'he9b5dba58189dbbc,
+65'h3956c25bf348b538, 65'h59f111f1b605d019 923f82a4af194f9b ab1c5ed5da6d8118
+d807aa98a3030242 12835b0145706fbe 243185be4ee4b28c 550c7dc3d5ffb4e2
+72be5d74f27b896f 80deb1fe3b1696b1 9bdc06a725c71235 c19bf174cf692694
+e49b69c19ef14ad2 efbe4786384f25e3 0fc19dc68b8cd5b5 240ca1cc77ac9c65
+2de92c6f592b0275 4a7484aa6ea6e483 5cb0a9dcbd41fbd4 76f988da831153b5
+983e5152ee66dfab a831c66d2db43210 b00327c898fb213f bf597fc7beef0ee4
+c6e00bf33da88fc2 d5a79147930aa725 06ca6351e003826f 142929670a0e6e70
+27b70a8546d22ffc 2e1b21385c26c926 4d2c6dfc5ac42aed 53380d139d95b3df
+650a73548baf63de 766a0abb3c77b2a8 81c2c92e47edaee6 92722c851482353b
+a2bfe8a14cf10364 a81a664bbc423001 c24b8b70d0f89791 c76c51a30654be30
+d192e819d6ef5218 d69906245565a910 f40e35855771202a 106aa07032bbd1b8
+19a4c116b8d2d0c8 1e376c085141ab53 2748774cdf8eeb99 34b0bcb5e19b48a8
+391c0cb3c5c95a63 4ed8aa4ae3418acb 5b9cca4f7763e373 682e6ff3d6b2b8a3
+748f82ee5defb2fc 78a5636f43172f60 84c87814a1f0ab72 8cc702081a6439ec
+90befffa23631e28 a4506cebde82bde9 bef9a3f7b2c67915 c67178f2e372532b
+ca273eceea26619c d186b8c721c0c207 eada7dd6cde0eb1e f57d4f7fee6ed178
+06f067aa72176fba 0a637dc5a2c898a6 113f9804bef90dae 1b710b35131c471b
+28db77f523047d84 32caab7b40c72493 3c9ebe0a15c9bebc 431d67c49c100d4c
+4cc5d4becb3e42b6 597f299cfc657e2a 5fcb6fab3ad6faec 6c44198c4a475817
+
+
+
+
+
+
+
+
+
+
+
+	
 
    // Define your intermediate variables here (forgetting them assumes variables are 1-bit)
 	//all must be size 64 bits, and now rounds go to 80 instead of 64, so 0 to 79 of each variable is needed
@@ -976,6 +1017,7 @@ module choice (input logic [63:0] x, y, z, output logic [63:0] ch);
    assign ch = (x & y) ^ (~x & z); 
 
 
+
 endmodule // choice
 
 module Sigma0 (input logic [63:0] x, output logic [63:0] Sig0);
@@ -987,6 +1029,7 @@ assign Sig0 = ({x[1:0], x[31:2]})^({x[12:0], x[31:13]})^({x[21:0], x[31:22]});
 //need to change to above equation 
 
 endmodule // Sigma0
+
 
 module sigma0 (input logic [63:0] x, output logic [63:0] sig0);
 
