f9b83d52e3d7bf74a27a7843ffc6d300a48e5d70
GunWill <gunnar.willis@okstate.edu>
2024-10-21 19:54:58 -0500

    Pretty sha512_tb.sv

--- Lab2/SV/sha512_tb.sv
+++ Lab2/SV/sha512_tb.sv
@@ -6,12 +6,12 @@ module stimulus;
    logic [MSG_SIZE-1:0] message;   
 	logic [511:0] hashed;
 	logic [511:0] golden;
-
+//Debugging:
   // assign golden = 512'hD693DB7749949506622261A533EF98E54ED5F60920F60AD03BC338D05BD9C90514919AE8B3DE1F25F7D99F87B0565D0A402493C5B40166A5EB7665C9E3ACAF2B;
 
    logic 	 clk;
 
-	//do we need to change these sizes?
+	
 	
 	logic [63:0]  errors;
 	logic [63:0]  vectornum;
@@ -25,7 +25,6 @@ module stimulus;
    integer       j;
 
    // Instantiate DUT
-	//num was 512 but chnaged to 1024, ????
 	
 	top #(MSG_SIZE, 1024) dut (message, hashed);
 
@@ -53,8 +52,9 @@ module stimulus;
         #0 result = 512'hD693DB7749949506622261A533EF98E54ED5F60920F60AD03BC338D05BD9C90514919AE8B3DE1F25F7D99F87B0565D0A402493C5B40166A5EB7665C9E3ACAF2B;
 	     #0 golden = 512'hD693DB7749949506622261A533EF98E54ED5F60920F60AD03BC338D05BD9C90514919AE8B3DE1F25F7D99F87B0565D0A402493C5B40166A5EB7665C9E3ACAF2B;
      
-     //"OSU ROCKS! YAY"
+	 //"OSU ROCKS! YAY"
   #200 message = 112'h4f535520524f434b532120594159;
+	//Expected result
      #0 result = 512'h4c3076f620c46d2a350fc0459f85d8454539ff314ad9b873a782e1b837da837c1a972eacea14cfcdec571b66957451574f664a65066412e2504ad3ae61749b0c;
      #0 golden = 512'h4c3076f620c46d2a350fc0459f85d8454539ff314ad9b873a782e1b837da837c1a972eacea14cfcdec571b66957451574f664a65066412e2504ad3ae61749b0c;
 
@@ -62,6 +62,7 @@ module stimulus;
      //"Christmas!!!!!"
 
   #200 message = 112'h4368726973746d61732121212121;
+	     //Expected result
      #0 result = 512'h9c8c5301dd51c49e3f0adb2cd24bf1df1219f5c5c6983dc208301f032f16119cd11f092886ffccdd702761d6ad52521bba6e076b66910a8c27bd2c836f9e4ec7;
      #0 golden = 512'h9c8c5301dd51c49e3f0adb2cd24bf1df1219f5c5c6983dc208301f032f16119cd11f092886ffccdd702761d6ad52521bba6e076b66910a8c27bd2c836f9e4ec7;
 
@@ -69,12 +70,14 @@ module stimulus;
      //"No More Tests!"
 
   #200 message = 112'h4e6f204d6f726520546573747321;
+	     //Expected Result
      #0 result = 512'h6ab4611f0729f0675686a4b661ebaef732b1a69c1c936f455b26d9a0f9ad4c133686aa13ae257841f0207ff053ead36649212ab22abc271f7ecf7c697fa20b9b;
      #0 golden = 512'h6ab4611f0729f0675686a4b661ebaef732b1a69c1c936f455b26d9a0f9ad4c133686aa13ae257841f0207ff053ead36649212ab22abc271f7ecf7c697fa20b9b;
 
      //"Thanksgiving!!"
 
   #200 message = 112'h5468616e6b73676976696e672121;
+	     //Expected result
      #0 result = 512'h8d5a91c2a2c50b4c18c43dbba7f43d2c4ec51033a2241fd39ce6f7430381f08bc84b4fbfee8fe5a433d2110e0052b721595b1794f35ff47151d4e7ee00f685d0;
      #0 golden = 512'h8d5a91c2a2c50b4c18c43dbba7f43d2c4ec51033a2241fd39ce6f7430381f08bc84b4fbfee8fe5a433d2110e0052b721595b1794f35ff47151d4e7ee00f685d0;
 
