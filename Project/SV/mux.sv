581e55693cdfd1009d54989e7e68be112d8177a3
emeliakravchuk <148596593+emeliakravchuk@users.noreply.github.com>
2024-11-19 11:59:26 -0600

    Update mux.sv

--- Project/SV/mux.sv
+++ Project/SV/mux.sv
@@ -29,7 +29,7 @@
 module mux2 #(parameter WIDTH = 32) (
   input  logic [WIDTH-1:0] d0, d1, 
   input  logic             s, 
-  output logic [31:0] y);
+  output logic [WIDTH-1:0] y);
 
   assign y = s ? d1 : d0; 
 endmodule
@@ -80,17 +80,16 @@ module mux16 #(parameter WIDTH = 8)
      endcase // case (s)
 endmodule // mux16
 
-/* verilator lint_on DECLFILENAME */
-module mux64 #(parameter WIDTH = 8)
-   (input logic [WIDTH-1:0] d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,
+module mux64 #(parameter WIDTH = 32)
+   (input logic [31:0] d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,
     d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24,
     d25, d26, d27, d28, d29, d30, d31, d32, d33, d34, d35, d36, d37, d38, 
     d39, d40, d41, d42, d43, d44, d45, d46, d47, d48, d49, d50, d51, d52,
-    d53, d54, d55, d56, d57, d58, d59, d60, d61, d62, d63, d64, input [5:0] s,
-    output logic [WIDTH-1:0] y);
+    d53, d54, d55, d56, d57, d58, d59, d60, d61, d62, d63, d64, input [5:0] count,
+    output logic [31:0] y);
 
    always_comb
-     case(s)
+     case(count)
        6'b000000: y = d0;
        6'b000001: y = d1;
        6'b000010: y = d2;
